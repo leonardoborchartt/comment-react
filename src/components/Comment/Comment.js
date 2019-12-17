656d1bb0068c41a17b9d1af7b4274210962466d6
Leonardo Borchartt <leob_b@hotmail.com>
2019-12-17 14:55:41 +0000

    send

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Comment.css'
 
-export default function Comment ({username, comment, time}){
+export default function Comment ({username, comment, localdate1}){
 	return (
 	<div className="comment">
 		<h4>{username} disse:</h4>
-		<p>{comment}</p>
+		<p>{comment}</p> <p>{localdate1}</p>
 		
 	</div>
 );
